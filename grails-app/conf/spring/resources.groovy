4c57ebdd72c92f1797d9be15b11cb2f41fb1263c
Shashank Agrawal <shagrawal99@gmail.com>
2017-06-02 14:27:09 +0530

    Installed spring-security-core and ran s2-quickstart.

--- grails-app/conf/spring/resources.groovy
+++ grails-app/conf/spring/resources.groovy
@@ -1,3 +1,5 @@
+import com.test.UserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
+    userPasswordEncoderListener(UserPasswordEncoderListener, ref('hibernateDatastore'))
 }
